88c00edcbdacf41f47b66752457fd72eb5234404
Pranay Gurav <guravpranay81@gmail.com>
2025-12-25 23:03:42 +0530

    logo updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,9 @@ const navLinks = [
   { label: "HOME", path: "/" },
   { label: "PLANS", path: "/plans" },
   { label: "SERVICES", path: "/services" },
-   {label:"PERSONAL TRAINING" ,path:"/personalTraining"},
+  { label: "PERSONAL TRAINING", path: "/personalTraining" },
   { label: "CONTACT", path: "/contact" },
- 
+
 ];
 
 const Navbar = () => {
@@ -49,12 +49,14 @@ const Navbar = () => {
         <div className="max-w-7xl mx-auto px-6 h-full flex items-center justify-between">
 
           {/* Logo */}
-          <Link to="/">
-            <img
-              src="/OMSAILOGO.webp"
-              alt="OM Sai The Fitness House"
-              className="h-10 w-auto object-contain"
-            />
+          <Link className="flex " to="/">
+
+            <svg className="relative" fill="#B11226" width="40px" height="40px" viewBox="0 0 50 50" version="1.2" baseProfile="tiny" xmlns="http://www.w3.org/2000/svg" overflow="inherit"><path d="M17.962 44.874c.374.403.352 1.041-.05 1.416l-2.172 2.031c-.402.375-1.037.353-1.411-.051l-12.649-13.632c-.374-.403-.351-1.04.051-1.416l2.175-2.028c.402-.376 1.037-.353 1.411.051l12.645 13.629zm16.14-25.65c.374.403.351 1.041-.051 1.416l-13.67 12.77c-.402.375-1.037.353-1.411-.051l-3.263-3.521c-.374-.403-.351-1.041.051-1.416l13.667-12.77c.401-.375 1.036-.353 1.41.051l3.267 3.521zm-11.489 21.303c.374.403.351 1.04-.051 1.416l-2.175 2.03c-.402.376-1.037.353-1.411-.051l-12.642-13.632c-.374-.403-.352-1.041.05-1.416l2.171-2.029c.402-.375 1.037-.353 1.411.051l12.647 13.631zm21.063-20.814c.374.403.351 1.041-.052 1.416l-2.174 2.03c-.402.375-1.037.353-1.412-.05l-12.644-13.629c-.375-.403-.352-1.04.05-1.416l2.18-2.035c.401-.375 1.036-.353 1.41.051l12.642 13.633zm4.644-4.34c.374.403.351 1.041-.051 1.417l-2.17 2.029c-.401.376-1.036.353-1.41-.05l-12.642-13.635c-.374-.403-.352-1.041.05-1.417l2.172-2.033c.401-.376 1.035-.354 1.409.05l12.642 13.639z" /></svg>
+            <div className="  text-[#B11226]">
+              <h3 className="font-heading text-[20px] text-start text-white italic ">OM SAI </h3>
+              <p className=" font-heading relative right-4 text-xs italic">The Fitness House</p>
+            </div>
+
           </Link>
 
           {/* Desktop Links */}
@@ -64,10 +66,9 @@ const Navbar = () => {
                 <NavLink
                   to={item.path}
                   className={({ isActive }) =>
-                    `transition ${
-                      isActive
-                        ? "text-[#B11226]"
-                        : "text-gray-200 hover:text-[#B11226]"
+                    `transition ${isActive
+                      ? "text-[#B11226]"
+                      : "text-gray-200 hover:text-[#B11226]"
                     }`
                   }
                 >
@@ -121,10 +122,9 @@ const Navbar = () => {
                 to={item.path}
                 onClick={() => setOpen(false)}
                 className={({ isActive }) =>
-                  `block transition ${
-                    isActive
-                      ? "text-[#B11226]"
-                      : "text-gray-200 hover:text-[#B11226]"
+                  `block transition ${isActive
+                    ? "text-[#B11226]"
+                    : "text-gray-200 hover:text-[#B11226]"
                   }`
                 }
               >
